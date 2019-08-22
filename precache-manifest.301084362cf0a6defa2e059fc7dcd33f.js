8689c12f79d389e68ebdef1cf3e7aecebaa449f1
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-22 10:14:15 -0600

    Updates

---
+++ precache-manifest.301084362cf0a6defa2e059fc7dcd33f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12f66d1ba502f0e5342dcf3d79505ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12a575db9433ea596a9",
+    "url": "/static/css/main.33b7cead.chunk.css"
+  },
+  {
+    "revision": "519a6db078aa2a3a4c44",
+    "url": "/static/js/2.66f1a293.chunk.js"
+  },
+  {
+    "revision": "c12a575db9433ea596a9",
+    "url": "/static/js/main.d3827c64.chunk.js"
+  },
+  {
+    "revision": "c0e853e31a41ad8eb8a8",
+    "url": "/static/js/runtime~main.da3aedd4.js"
+  },
+  {
+    "revision": "45b0e69add05f7cdbee40cc9997bd7b4",
+    "url": "/static/media/amazon.45b0e69a.png"
+  },
+  {
+    "revision": "55ec8139118cec62241cf56c427edc00",
+    "url": "/static/media/facebook.55ec8139.png"
+  },
+  {
+    "revision": "751a308e301c42b19e6706d859bc0abb",
+    "url": "/static/media/instagram.751a308e.png"
+  },
+  {
+    "revision": "af11d6a617d8c857091d30be0755de21",
+    "url": "/static/media/me.af11d6a6.png"
+  },
+  {
+    "revision": "6de2b97bcc24e8517c80d800661a92ac",
+    "url": "/static/media/snapchat.6de2b97b.png"
+  },
+  {
+    "revision": "f925d88ae9600259100aea163a4862bf",
+    "url": "/static/media/static.f925d88a.png"
+  }
+]);
