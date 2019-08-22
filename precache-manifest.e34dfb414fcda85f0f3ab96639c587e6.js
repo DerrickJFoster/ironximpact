7cc758f1cc0e4703dda867e020748b5a72caaccd
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-22 13:04:32 -0600

    Updates

---
+++ precache-manifest.e34dfb414fcda85f0f3ab96639c587e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14e2950091bf106ed5fb560a2db2f5a",
+    "url": "/ironximpact/index.html"
+  },
+  {
+    "revision": "1189075393ef339a2eb9",
+    "url": "/ironximpact/static/css/main.34ca176a.chunk.css"
+  },
+  {
+    "revision": "456221285afe1b4ee32d",
+    "url": "/ironximpact/static/js/2.5dddd37a.chunk.js"
+  },
+  {
+    "revision": "1189075393ef339a2eb9",
+    "url": "/ironximpact/static/js/main.4f47061c.chunk.js"
+  },
+  {
+    "revision": "56600b00abfbf714274e",
+    "url": "/ironximpact/static/js/runtime~main.da53515a.js"
+  },
+  {
+    "revision": "45b0e69add05f7cdbee40cc9997bd7b4",
+    "url": "/ironximpact/static/media/amazon.45b0e69a.png"
+  },
+  {
+    "revision": "55ec8139118cec62241cf56c427edc00",
+    "url": "/ironximpact/static/media/facebook.55ec8139.png"
+  },
+  {
+    "revision": "751a308e301c42b19e6706d859bc0abb",
+    "url": "/ironximpact/static/media/instagram.751a308e.png"
+  },
+  {
+    "revision": "af11d6a617d8c857091d30be0755de21",
+    "url": "/ironximpact/static/media/me.af11d6a6.png"
+  },
+  {
+    "revision": "6de2b97bcc24e8517c80d800661a92ac",
+    "url": "/ironximpact/static/media/snapchat.6de2b97b.png"
+  },
+  {
+    "revision": "f925d88ae9600259100aea163a4862bf",
+    "url": "/ironximpact/static/media/static.f925d88a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8875f9501f8cbb13efab95f0bf4b24d.js"
+  "/ironximpact/precache-manifest.e34dfb414fcda85f0f3ab96639c587e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ironximpact/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
