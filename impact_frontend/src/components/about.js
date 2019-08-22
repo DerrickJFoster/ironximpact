7e15057826baee5a6de213447f968f147558e278
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-21 18:34:36 -0600

    Changing page header styling

--- impact_frontend/src/components/about.js
+++ impact_frontend/src/components/about.js
@@ -9,13 +9,6 @@ class About extends Component {
       <h1 className='about'>About Me</h1>
         <img id='profile' src={Me} />
           <p>I am a small time variety streamer living in Denver Colorado. I am a Software Engineer/Web Developer and I like to game for fun in my spare time. I've always had a passion for electronics and video games which is what drove me to my career as well as twitch streaming as a hobby.
-
-          Feel free to check out more of my social media here at:
-      <br/>
-      <br/>
-      Instagram: <a href="https://instagram.com/IronxImpact">@IronxImpact</a>
-      <br/>
-      Amazon Wishlist: <a href="https://www.amazon.com/hz/wishlist/ls/1GB2CXB65NLXY?ref_=wl_share">here</a>
       </p>
       </div>
       </>
--- impact_frontend/src/components/home.js
+++ impact_frontend/src/components/home.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import '../App.css'
 import Me from '../images/me.png'
+import instagram from '../images/instagram.png'
+import facebook from '../images/facebook.png'
+import snapchat from '../images/snapchat.png'
+import amazon from '../images/amazon.png'
 
 class Home extends Component {
   render() {
     return (
       <>
       <div className='social-media'>
-      <h1 className='about'>Social Media</h1>
-        <img id='profile-widget' src={Me} />
+        <h1 className='about'>Social Media</h1>
+        {/* <img id='profile-widget' src={Me} /> */}
 
-        <a className="twitter-timeline" data-width="220" data-height="300" data-theme="dark" data-link-color="#981CEB" href="https://twitter.com/IronxImpact?ref_src=twsrc%5Etfw">Tweets by IronxImpact</a>
+          <a className="twitter-timeline" data-width="220" data-height="300" data-theme="dark" data-link-color="#981CEB" href="https://twitter.com/IronxImpact?ref_src=twsrc%5Etfw">Tweets by IronxImpact</a>
+
+          <div className='icons'>
+            <a href='http://www.instagram.com/IronxImpact'><img  className='insta' src={instagram} /></a>
+            <a href='#'><img  className='fb' src={facebook} /></a>
+            <a href='#'><img  className='snap' src={snapchat} /></a>
+            <a href="https://www.amazon.com/hz/wishlist/ls/1GB2CXB65NLXY?ref_=wl_share"><img className='insta' src={amazon} /></a>
+          </div>
       </div>
 
       <iframe className='video'
